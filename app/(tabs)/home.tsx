d38b6f2acab03d94b5c9ed3f60e2589248059aa6
IjonK007 <kushta.joni@gmail.com>
2024-10-09 17:38:35 +0200

    home screen design

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,9 +1,41 @@
-import { View, Text } from 'react-native'
+import images from '@/constants/images'
+import { View, Text, FlatList, Image } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className='bg-primary'>
+      <FlatList 
+        data={[{id: 1}, {id: 2}, {id: 3}]}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <Text className='text-3xl text-white'>{item.id}</Text>
+        )}
+        ListHeaderComponent={() => (
+          <View className='my-6 px-4 space-y-6'>
+            <View className='justify-between items-start flex-row mb-6'>
+              <View>
+                <Text className='font-pmedium text-sm text-gray-100'>
+                  Welcome Back
+                </Text>
+                <Text className='text-2xl font-psemibold text-white'>
+                  Top Lali
+                </Text>
+              </View>
+
+              <View className='mt-1.5'>
+                <Image 
+                  source={images.logoSmall}
+                  className='w-9 h-10'
+                  resizeMode='contain'
+                />
+              </View>
+            </View>
+
+            
+          </View>
+        )}
+      />
+    </SafeAreaView>
   )
 }
 export default Home
